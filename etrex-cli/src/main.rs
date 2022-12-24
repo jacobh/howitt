7a21b8817f83a3f0073dec0b303a08855fc92e3a
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2022-12-24 16:56:32 +1100

    split out load functions

--- etrex-cli/src/main.rs
+++ etrex-cli/src/main.rs
@@ -1,10 +1,10 @@
 use std::{
-    fs,
-    path::{Path, PathBuf}, io,
+    fs, io,
+    path::{Path, PathBuf},
 };
 
 use clap::{Args, Parser, Subcommand};
-use etrex::{gtfs::GtfsZip, trip::detect_trips, EtrexFile, checkpoint::Checkpoint};
+use etrex::{checkpoint::Checkpoint, gtfs::GtfsZip, trip::detect_trips, EtrexFile};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 #[derive(Parser)]
@@ -52,6 +52,41 @@ fn find_file_paths(dirpath: &Path) -> Vec<PathBuf> {
         .collect()
 }
 
+fn load_stations(ptv_gtfs_dirpath: &Path) -> Result<Vec<Checkpoint>, anyhow::Error> {
+    let file_paths: Vec<PathBuf> = find_file_paths(ptv_gtfs_dirpath)
+        .into_iter()
+        .filter(|path| path.extension() == Some("zip".as_ref()))
+        .collect();
+
+    let gtfs_zips: Vec<GtfsZip> = file_paths
+        .into_iter()
+        .map(|path| -> Result<_, anyhow::Error> {
+            let data = fs::read(path)?;
+            let cursor = io::Cursor::new(data);
+            Ok(GtfsZip::parse(cursor)?)
+        })
+        .collect::<Result<_, _>>()?;
+
+    let checkpoints = gtfs_zips
+        .into_iter()
+        .flat_map(|zip| zip.stops)
+        .map(Checkpoint::from);
+    Ok(checkpoints
+        .filter(|checkpoint| checkpoint.name.contains("Railway Station"))
+        .collect::<Vec<_>>())
+}
+
+fn load_huts(filepath: &Path) -> Result<Vec<Checkpoint>, anyhow::Error> {
+    let data = fs::read(filepath)?;
+    let file = EtrexFile::parse(&data)?;
+    Ok(file
+        .gpx
+        .waypoints
+        .into_iter()
+        .map(Checkpoint::try_from)
+        .collect::<Result<Vec<_>, _>>()?)
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
 
@@ -81,28 +116,11 @@ fn main() -> Result<(), anyhow::Error> {
             }
         }
         Commands::Stations(args) => {
-            let file_paths: Vec<PathBuf> = find_file_paths(&args.ptv_gtfs_dirpath)
-                .into_iter()
-                .filter(|path| path.extension() == Some("zip".as_ref()))
-                .collect();
-
-            let gtfs_zips: Vec<GtfsZip> = file_paths
-                .into_iter()
-                .map(|path| -> Result<_, anyhow::Error> {
-                    let data = fs::read(path)?;
-                    let cursor = io::Cursor::new(data);
-                    Ok(GtfsZip::parse(cursor)?)
-                })
-                .collect::<Result<_, _>>()?;
-
-            let checkpoints = gtfs_zips.into_iter().flat_map(|zip| zip.stops).map(Checkpoint::from);
-            let railway_stations = checkpoints.filter(|checkpoint| checkpoint.name.contains("Railway Station")).collect::<Vec<_>>();
+            let railway_stations = load_stations(&args.ptv_gtfs_dirpath)?;
             dbg!(railway_stations);
         }
         Commands::Huts(args) => {
-            let data = fs::read(&args.filepath)?;
-            let file = EtrexFile::parse(&data)?;
-            let huts = file.gpx.waypoints.into_iter().map(Checkpoint::try_from).collect::<Result<Vec<_>, _>>()?;
+            let huts = load_huts(&args.filepath)?;
             dbg!(huts);
         }
     }
