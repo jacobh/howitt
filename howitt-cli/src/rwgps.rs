24e5741ef3474708f0e3e5f7fbef865c63921143
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2022-12-25 20:01:38 +0800

    split out user config fns

--- howitt-cli/src/rwgps.rs
+++ howitt-cli/src/rwgps.rs
@@ -1,22 +1,22 @@
 use clap::Subcommand;
-use rwgps::{PasswordAuthInfo, TokenAuthInfo, AuthInfo};
+use rwgps::{AuthInfo, PasswordAuthInfo, TokenAuthInfo};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::{dirs::CONFIG_DIRPATH, json::prettyprintln};
 
-const AUTH_FILENAME: &'static str = "rwgps_auth.toml";
+const CONFIG_FILENAME: &'static str = "rwgps_auth.toml";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct UserConfig {
     password_info: PasswordAuthInfo,
-    user_info: Option<UserInfo>
+    user_info: Option<UserInfo>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct UserInfo {
     user_id: usize,
-    token_info: TokenAuthInfo
+    token_info: TokenAuthInfo,
 }
 
 #[derive(Subcommand)]
@@ -25,50 +25,67 @@ pub enum Rwgps {
     Auth,
 }
 
+fn get_user_config() -> Result<UserConfig, anyhow::Error> {
+    let config_filepath = CONFIG_DIRPATH.join(CONFIG_FILENAME);
+
+    match config_filepath.exists() {
+        true => Ok(toml::from_slice(&std::fs::read(&config_filepath)?)?),
+        false => {
+            println!("Initial user setup");
+            let email = inquire::Text::new("Email").prompt();
+            let password = inquire::Password::new("Password")
+                .without_confirmation()
+                .prompt();
+
+            match (email, password) {
+                (Ok(email), Ok(password)) => Ok(UserConfig {
+                    password_info: PasswordAuthInfo { email, password },
+                    user_info: None,
+                }),
+                _ => anyhow::bail!("Invalid email/password"),
+            }
+        }
+    }
+}
+
+fn persist_user_config(config: &UserConfig) -> Result<(), anyhow::Error> {
+    let config_filepath = CONFIG_DIRPATH.join(CONFIG_FILENAME);
+
+    std::fs::write(config_filepath, toml::to_vec(config)?)?;
+
+    Ok(())
+}
+
 pub async fn handle(command: &Rwgps) -> Result<(), anyhow::Error> {
+    let client = rwgps::RwgpsClient::new();
+
     match command {
         Rwgps::Info => {
             println!("hello")
         }
         Rwgps::Auth => {
-            let auth_filepath = CONFIG_DIRPATH.join(AUTH_FILENAME);
-
-            let auth_config: UserConfig = match auth_filepath.exists() {
-                true => toml::from_slice(&std::fs::read(&auth_filepath)?)?,
-                false => {
-                    println!("Initial auth setup");
-                    let email = inquire::Text::new("Email").prompt();
-                    let password = inquire::Password::new("Password")
-                        .without_confirmation()
-                        .prompt();
-
-                    match (email, password) {
-                        (Ok(email), Ok(password)) => UserConfig {
-                            password_info: PasswordAuthInfo { email, password },
-                            user_info: None
-                        },
-                        _ => anyhow::bail!("Invalid email/password"),
-                    }
-                }
-            };
-
-            let client = rwgps::RwgpsClient::new();
+            let user_config = get_user_config()?;
 
             let auth_resp = client
-                .user_info(&AuthInfo::from(auth_config.password_info.clone()))
+                .user_info(&AuthInfo::from(user_config.password_info.clone()))
                 .await?;
 
-            let updated_auth_config = UserConfig {
-                user_info: Some(UserInfo { user_id: auth_resp.user.id, token_info: TokenAuthInfo { auth_token: auth_resp.user.auth_token } }),
-                ..auth_config
+            let updated_user_config = UserConfig {
+                user_info: Some(UserInfo {
+                    user_id: auth_resp.user.id,
+                    token_info: TokenAuthInfo {
+                        auth_token: auth_resp.user.auth_token,
+                    },
+                }),
+                ..user_config
             };
 
-            std::fs::write(auth_filepath, toml::to_vec(&updated_auth_config)?)?;
+            persist_user_config(&updated_user_config)?;
 
             prettyprintln(json!({
-                "email": updated_auth_config.password_info.email,
+                "email": updated_user_config.password_info.email,
                 "password": "********",
-                "user_info": updated_auth_config.user_info,
+                "user_info": updated_user_config.user_info,
             }));
         }
     }
