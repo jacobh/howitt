6e8fda5747c2a75749faa7a260ff1ccdaf0cc601
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2022-12-28 15:56:38 +0800

    async graphql scaffold

--- howitt-web/src/main.rs
+++ howitt-web/src/main.rs
@@ -1,12 +1,51 @@
-use warp::Filter;
+use std::convert::Infallible;
+
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
+use http::StatusCode;
+use warp::{http::Response as HttpResponse, Filter, Rejection};
+
+use crate::graphql::Query;
+
+mod graphql;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .finish();
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    let graphql_post = async_graphql_warp::graphql(schema).and_then(
+        |(schema, request): (
+            Schema<Query, EmptyMutation, EmptySubscription>,
+            async_graphql::Request,
+        )| async move {
+            Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+        },
+    );
+
+    let graphiql = warp::path::end().and(warp::get()).map(|| {
+        HttpResponse::builder()
+            .header("content-type", "text/html")
+            .body(GraphiQLSource::build().endpoint("/").finish())
+    });
+
+    let routes = graphiql
+        .or(graphql_post)
+        .recover(|err: Rejection| async move {
+            if let Some(GraphQLBadRequest(err)) = err.find() {
+                return Ok::<_, Infallible>(warp::reply::with_status(
+                    err.to_string(),
+                    StatusCode::BAD_REQUEST,
+                ));
+            }
+
+            Ok(warp::reply::with_status(
+                "INTERNAL_SERVER_ERROR".to_string(),
+                StatusCode::INTERNAL_SERVER_ERROR,
+            ))
+        });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
