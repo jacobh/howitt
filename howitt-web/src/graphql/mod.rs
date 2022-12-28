0664d496e53b6f926cb23693c4ef2c032b4a9d1e
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2022-12-28 16:10:46 +0800

    scaffold schema

--- howitt-web/src/graphql/mod.rs
+++ howitt-web/src/graphql/mod.rs
@@ -4,7 +4,59 @@ pub struct Query;
 
 #[Object]
 impl Query {
-    async fn howdy(&self) -> &'static str {
-        "partner"
+    async fn routes(&self) -> Vec<Route> {
+        vec![]
     }
+    async fn route(&self, _ctx: &Context<'_>, _id: usize) -> Option<Route> {
+        None
+    }
+    async fn checkpoints(&self) -> Vec<Checkpoint> {
+        vec![]
+    }
+    async fn checkpoint(&self, _ctx: &Context<'_>, _id: usize) -> Option<Checkpoint> {
+        None
+    }
+}
+
+
+pub struct Route;
+
+#[Object]
+impl Route {
+    async fn id(&self) -> usize {
+        1
+    }
+    async fn points(&self) -> Vec<Point> {
+        vec![]
+    }
+}
+
+pub struct Checkpoint;
+
+#[Object]
+impl Checkpoint {
+    async fn id(&self) -> usize {
+        1
+    }
+    async fn point(&self) -> Point {
+        Point {lat: 0.0, lng: 0.0}
+    }
+}
+
+pub struct Segment;
+
+#[Object]
+impl Segment {
+    async fn id(&self) -> usize {
+        1
+    }
+    async fn points(&self) -> Vec<Point> {
+        vec![]
+    }
+}
+
+#[derive(SimpleObject)]
+struct Point {
+    lat: f64,
+    lng: f64
 }
