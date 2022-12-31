c9a4e91ba8afa4ef3ab0ce9a3641d4fb51e38028
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2022-12-31 11:45:39 +0800

    start setting up overlay

--- webui/src/pages/index.tsx
+++ webui/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
+import { Hanken_Grotesk } from "@next/font/google";
 import { Map } from "../components/map";
 import styled from "styled-components";
 import { useQuery } from "@apollo/client";
 import { gql } from "../__generated__/gql";
+import { Card } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const hanken = Hanken_Grotesk();
 
 const StyledMain = styled.main`
   width: 100%;
@@ -30,8 +33,15 @@ const STARRED_ROUTES_QUERY = gql(`
   }
 `);
 
+const OverlayContainer = styled.div`
+  position: fixed;
+  z-index: 10;
+  width: 100%;
+`;
+
 export default function Home() {
   const { loading, data } = useQuery(STARRED_ROUTES_QUERY);
+  const [inspectedFeatures, setInspectedFeatures] = useState<any[]>([]);
 
   return (
     <>
@@ -40,9 +50,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <StyledMain>
-        <Map routes={data?.starredRoutes} checkpoints={data?.checkpoints} />
-      </StyledMain>
+      <div className={hanken.className}>
+        <StyledMain>
+          <OverlayContainer>
+            <Grid2 container spacing={2}>
+              <Grid2 xs={4} xsOffset={8}>
+                {inspectedFeatures.length > 0 && (
+                  <Card>
+                    <h2>Inspect Info</h2>
+                  </Card>
+                )}
+              </Grid2>
+            </Grid2>
+          </OverlayContainer>
+          <Map routes={data?.starredRoutes} checkpoints={data?.checkpoints} />
+        </StyledMain>
+      </div>
     </>
   );
 }
