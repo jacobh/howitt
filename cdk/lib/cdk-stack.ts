0397c5d1e014a691f294e1c22e8478bec0e2e21e
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2023-02-05 20:14:03 +1100

    set up custom domain

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { RustFunction } from 'cargo-lambda-cdk';
-import path = require('path');
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { RustFunction } from "cargo-lambda-cdk";
+import path = require("path");
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
 
-const PROJECT_ROOT_DIR = path.resolve(__dirname, '../..')
+const PROJECT_ROOT_DIR = path.resolve(__dirname, "../..");
 
-console.log(PROJECT_ROOT_DIR)
+console.log(PROJECT_ROOT_DIR);
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -20,16 +24,26 @@ export class CdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-    const webLambda = new RustFunction(this, 'howitt-web-lambda', {
+    const certificate = new Certificate(this, "howitt-api.haslehurst.net", {
+      domainName: "howitt-api.haslehurst.net",
+      validation: CertificateValidation.fromDns(),
+    })
+
+    const webLambda = new RustFunction(this, "howitt-web-lambda", {
       manifestPath: PROJECT_ROOT_DIR,
     });
 
-    const webLambdaIntegration = new LambdaIntegration(webLambda)
+    const webLambdaIntegration = new LambdaIntegration(webLambda);
 
     const api = new RestApi(this, "howitt-api", {
       restApiName: "Howitt API",
+      disableExecuteApiEndpoint: true,
+      domainName: {
+        domainName: "howitt-api.haslehurst.net",
+        certificate,
+      },
     });
 
-    api.root.addProxy({ defaultIntegration: webLambdaIntegration })
+    api.root.addProxy({ defaultIntegration: webLambdaIntegration });
   }
 }
