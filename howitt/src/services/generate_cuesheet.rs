6665e6cd7a463f4753803221ecf3695f49489875
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2023-05-06 20:33:36 +1000

    WIP

--- howitt/src/services/generate_cuesheet.rs
+++ howitt/src/services/generate_cuesheet.rs
@@ -1,9 +1,43 @@
-use crate::models::{point::{ElevationPoint, Point}, point_of_interest::PointOfInterest, cuesheet::Cuesheet};
+use itertools::{Itertools, Position};
+
+use crate::models::{
+    cuesheet::Cuesheet, point::ElevationPoint, point_of_interest::PointOfInterest,
+};
 
 use super::nearby::nearby_points_of_interest;
 
-pub fn generate_cuesheet(route: &[Point], pois: &[PointOfInterest]) -> Cuesheet {
-    let nearby_pois = nearby_points_of_interest(route, pois, 500);
+pub fn generate_cuesheet<P>(route: &[ElevationPoint], pois: &[PointOfInterest]) -> Cuesheet {
+    let nearby_pois = nearby_points_of_interest(route, pois, 500.0);
+
+    let partitioned_points = route
+        .iter()
+        .with_position()
+        .map(|point| match point {
+            Position::First(point) | Position::Middle(point) => (point, false),
+            Position::Last(point) | Position::Only(point) => (point, true),
+        })
+        .scan::<Vec<&ElevationPoint>, _, _>(vec![], |state, (point, is_last)| {
+            state.push(point);
+
+            let nearby_poi = nearby_pois
+                .iter()
+                .find(|nearby| nearby.closest_point.as_ref() == point);
+
+            match nearby_poi {
+                Some(nearby_poi) => {
+                    let points = std::mem::replace(state, vec![]);
+                    Some((points, Some(nearby_poi)))
+                }
+                None => {
+                    if is_last {
+                        let points = std::mem::replace(state, vec![]);
+                        Some((points, None))
+                    } else {
+                        None
+                    }
+                }
+            }
+        });
 
     Cuesheet { cues: vec![] }
 }
--- howitt/src/services/mod.rs
+++ howitt/src/services/mod.rs
@@ -1,3 +1,4 @@
 pub mod detect_segments;
+pub mod generate_cuesheet;
 pub mod nearby;
 pub mod rwgps;
