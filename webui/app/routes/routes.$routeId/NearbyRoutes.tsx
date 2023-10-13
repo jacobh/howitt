4a19a111a89f256ec8c50e4a6bdb24d7c8aa25be
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2023-10-14 10:36:49 +1100

    basic styling for nearby routes

--- webui/app/routes/routes.$routeId/NearbyRoutes.tsx
+++ webui/app/routes/routes.$routeId/NearbyRoutes.tsx
@@ -2,6 +2,7 @@ import { Link } from "@remix-run/react";
 import { NearbyRoute, Route, Terminus } from "~/__generated__/graphql";
 import { formatDistance } from "~/services/format";
 import { CardinalSubset, cardinalFromDegree } from "cardinal-direction";
+import { COLORS } from "~/styles/theme";
 
 interface Props {
   terminus: Pick<Terminus, "bearing">;
@@ -17,20 +18,20 @@ export function NearbyRoutes({
   nearbyRoutes,
 }: Props): React.ReactElement {
   return (
-    <div>
-      <h3>
+    <div css={{ margin: "20px 0" }}>
+      <p css={{ margin: "10px 0" }}>
         Nearby Routes (
         {cardinalFromDegree(terminus.bearing, CardinalSubset.Ordinal)})
-      </h3>
+      </p>
       {nearbyRoutes.map(({ delta, closestTerminus: { bearing, route } }) => (
-        <div key={route.id}>
-          <h3>
+        <div key={route.id} css={{ margin: "10px 0" }}>
+          <p>
             <Link to={`/routes/${route.id.split("#")[1]}`}>
               {route.name} (
               {cardinalFromDegree(bearing, CardinalSubset.Ordinal)})
             </Link>
-          </h3>
-          <p>
+          </p>
+          <p css={{ color: COLORS.darkGrey }}>
             {formatDistance(delta.distance)}{" "}
             {cardinalFromDegree(delta.bearing, CardinalSubset.Ordinal)}
           </p>
