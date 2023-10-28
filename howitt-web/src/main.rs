7208e2db45c3596aab406113b96ed63fbd41c29b
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2023-10-28 14:21:18 +1100

    fix local server

--- howitt-web/src/main.rs
+++ howitt-web/src/main.rs
@@ -4,7 +4,11 @@ use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Sche
 use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
 use howitt::repos::{CachingRepo, ConfigRepo, PointOfInterestRepo, RideModelRepo, RouteModelRepo};
 use howitt_dynamo::SingleTableClient;
-use howitt_graphql::{context::SchemaData, Query};
+use howitt_graphql::{
+    context::{RequestData, SchemaData},
+    credentials::Credentials,
+    Query,
+};
 use http::StatusCode;
 use warp::{http::Response as HttpResponse, Filter, Rejection};
 
@@ -39,16 +43,27 @@ async fn main() -> Result<(), anyhow::Error> {
     let cors = warp::cors()
         .allow_any_origin()
         .allow_methods(vec!["GET", "POST"])
-        .allow_headers(vec!["content-type"]);
+        .allow_headers(vec!["content-type", "authorization"]);
 
-    let graphql_post = async_graphql_warp::graphql(schema).and_then(
-        |(schema, request): (
-            Schema<Query, EmptyMutation, EmptySubscription>,
-            async_graphql::Request,
-        )| async move {
-            Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
-        },
-    );
+    let auth_header_filter =
+        warp::header::optional::<String>("authorization").map(|auth_header: Option<String>| {
+            auth_header
+                .as_deref()
+                .and_then(|s| Credentials::parse_auth_header_value(s).ok())
+        });
+
+    let graphql_post = auth_header_filter
+        .and(async_graphql_warp::graphql(schema))
+        .and_then(
+            |credentials,
+             (schema, mut request): (
+                Schema<Query, EmptyMutation, EmptySubscription>,
+                async_graphql::Request,
+            )| async move {
+                request = request.data(RequestData { credentials });
+                Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+            },
+        );
 
     let graphiql = warp::path::end().and(warp::get()).map(|| {
         HttpResponse::builder()
