4db77e6ffdc28ab6c0b41b189aa7c98420bfc69c
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2024-05-09 19:31:14 +1000

    start building query

--- howitt-hs/app/Main.hs
+++ howitt-hs/app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Amazonka.DynamoDB as DynamoDB
+import qualified Data.Text as T
+
+-- testQuery :: Text -> Query
+
+
+tableName :: T.Text
+tableName = T.pack "howitt"
+
+testQuery :: Query
+testQuery = newQuery tableName
+
+-- queryString :: BufferCodec from to state -> c
+-- queryString = testQuery . toJSON . encode
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "hello"
