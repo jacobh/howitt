4cfb60752c3c922460e589edd4ba19fa699f699c
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2024-05-10 12:29:43 +1000

    WIP

--- howitt-hs/app/Main.hs
+++ howitt-hs/app/Main.hs
@@ -1,19 +1,25 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Main where
 
+-- import Amazonka
 import Amazonka.DynamoDB as DynamoDB
-import qualified Data.Text as T
+import Data.Function
+import Data.Generics.Internal.VL.Lens
+import Data.Generics.Product
+import Data.HashMap.Strict qualified as HashMap
+import Data.Text qualified as T
 
--- testQuery :: Text -> Query
-
-
-tableName :: T.Text
-tableName = T.pack "howitt"
-
-testQuery :: Query
-testQuery = newQuery tableName
-
--- queryString :: BufferCodec from to state -> c
--- queryString = testQuery . toJSON . encode
+queryPk :: T.Text -> Query
+queryPk pk = newQuery (T.pack "howitt")
+    & field @"keyConditionExpression" .~ Just (T.pack "#pk = :pk")
+    & field @"expressionAttributeNames" .~ Just (HashMap.fromList [(T.pack "#pk", T.pack "pk")])
+    & field @"expressionAttributeValues" .~ Just (HashMap.fromList [(T.pack ":pk", DynamoDB.S pk)])
 
 main :: IO ()
 main = putStrLn "hello"
