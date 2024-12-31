e44e4c6f16228c59055dbd031c2fe1088aaf2088
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2024-12-31 14:40:28 +1100

    strip unused deps

--- howitt/src/ext/serde.rs
+++ howitt/src/ext/serde.rs
@@ -17,36 +17,6 @@ pub mod point_tuple {
     }
 }
 
-pub mod compressed_bytes {
-    use serde::{de::DeserializeOwned, Deserialize, Serialize};
-
-    pub fn serialize<T: Serialize, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut encoder = snap::raw::Encoder::new();
-
-        let bytes = bincode::serialize(value).map_err(serde::ser::Error::custom)?;
-        let compressed_bytes = encoder
-            .compress_vec(&bytes)
-            .map_err(serde::ser::Error::custom)?;
-        compressed_bytes.serialize(serializer)
-    }
-
-    pub fn deserialize<'de, T: DeserializeOwned, D>(deserializer: D) -> Result<T, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let mut decoder = snap::raw::Decoder::new();
-
-        let compressed_bytes: Vec<u8> = Deserialize::deserialize(deserializer)?;
-        let bytes = decoder
-            .decompress_vec(&compressed_bytes)
-            .map_err(serde::de::Error::custom)?;
-        bincode::deserialize(&bytes).map_err(serde::de::Error::custom)
-    }
-}
-
 pub mod json {
     pub fn into_string_value(value: serde_json::Value) -> Option<String> {
         match value {
