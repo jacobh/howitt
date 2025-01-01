a2bd0bb8906ece21081e58d585b87dbede978370
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-01 11:45:00 +1100

    impl RouteIndexRow

--- howitt-postgresql/src/repos/route_repo.rs
+++ howitt-postgresql/src/repos/route_repo.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use chrono::{DateTime, Utc};
 use howitt::ext::iter::ResultIterExt;
 use howitt::ext::serde::json::unwrap_string_value;
@@ -15,6 +13,78 @@ use howitt::repos::Repo;
 use uuid::Uuid;
 
 use crate::{PostgresClient, PostgresRepoError};
+
+struct RouteIndexRow {
+    id: Uuid,
+    created_at: DateTime<Utc>,
+    name: String,
+    external_ref: Option<serde_json::Value>,
+    distance_m: i32,
+    sample_points: serde_json::Value,
+    description: Option<String>,
+    published_at: Option<DateTime<Utc>>,
+    technical_difficulty: Option<String>,
+    physical_difficulty: Option<String>,
+    minimum_bike: Option<serde_json::Value>,
+    ideal_bike: Option<serde_json::Value>,
+    scouted: Option<String>,
+    direction: Option<String>,
+    tags: Vec<String>,
+    is_starred: bool,
+}
+
+impl TryFrom<RouteIndexRow> for Route {
+    type Error = PostgresRepoError;
+
+    fn try_from(row: RouteIndexRow) -> Result<Self, Self::Error> {
+        Ok(Route {
+            id: sqlx::Either::Left(uuid_into_ulid(row.id)),
+            name: row.name,
+            distance: row.distance_m as f64,
+            sample_points: Some(serde_json::from_value(row.sample_points)?),
+            description: Some(RouteDescription {
+                description: row.description,
+                published_at: row.published_at,
+                technical_difficulty: row
+                    .technical_difficulty
+                    .map(serde_json::Value::String)
+                    .map(serde_json::from_value)
+                    .transpose()?,
+                physical_difficulty: row
+                    .physical_difficulty
+                    .map(serde_json::Value::String)
+                    .map(serde_json::from_value)
+                    .transpose()?,
+                minimum_bike: row.minimum_bike.map(serde_json::from_value).transpose()?,
+                ideal_bike: row.ideal_bike.map(serde_json::from_value).transpose()?,
+                scouted: row
+                    .scouted
+                    .map(serde_json::Value::String)
+                    .map(serde_json::from_value)
+                    .transpose()?,
+                direction: row
+                    .direction
+                    .map(serde_json::Value::String)
+                    .map(serde_json::from_value)
+                    .transpose()?,
+                tags: row.tags.clone(),
+            }),
+            external_ref: row.external_ref.map(serde_json::from_value).transpose()?,
+            tags: std::iter::empty()
+                .chain(row.tags.into_iter().map(Tag::Custom))
+                .chain(
+                    if row.is_starred {
+                        vec![Tag::Starred]
+                    } else {
+                        vec![]
+                    }
+                    .into_iter(),
+                )
+                .into_iter()
+                .collect(),
+        })
+    }
+}
 
 struct RouteRow {
     id: Uuid,
@@ -142,7 +212,10 @@ impl Repo for PostgresRouteRepo {
     ) -> Result<Vec<<RouteModel as Model>::IndexItem>, PostgresRepoError> {
         let mut conn = self.client.acquire().await.unwrap();
 
-        let query = sqlx::query_as!(RouteRow, r#"select * from routes"#);
+        let query = sqlx::query_as!(
+            RouteIndexRow,
+            r#"select id, created_at, name, external_ref, distance_m, sample_points, description, published_at, technical_difficulty, physical_difficulty, minimum_bike, ideal_bike, scouted, direction, tags, is_starred from routes"#
+        );
 
         Ok(query
             .fetch_all(conn.as_mut())
@@ -166,7 +239,15 @@ impl Repo for PostgresRouteRepo {
         &self,
         id: <RouteModel as Model>::Id,
     ) -> Result<<RouteModel as Model>::IndexItem, PostgresRepoError> {
-        Ok(self.get(id).await?.route)
+        let mut conn = self.client.acquire().await.unwrap();
+
+        let query = sqlx::query_as!(
+            RouteIndexRow,
+            r#"select id, created_at, name, external_ref, distance_m, sample_points, description, published_at, technical_difficulty, physical_difficulty, minimum_bike, ideal_bike, scouted, direction, tags, is_starred  from routes where id = $1"#,
+            Uuid::from(id)
+        );
+
+        Ok(Route::try_from(query.fetch_one(conn.as_mut()).await?)?)
     }
     async fn put(&self, model: RouteModel) -> Result<(), PostgresRepoError> {
         let mut conn = self.client.acquire().await.unwrap();
