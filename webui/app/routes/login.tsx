0920a486fd433690bb98666d43052f37d391e69d
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-02 13:01:31 +1100

    impl login

--- webui/app/routes/login.tsx
+++ webui/app/routes/login.tsx
@@ -1,32 +1,105 @@
-import { useCallback, useEffect, useState } from "react";
+import { css } from "@emotion/react";
+import { useNavigate } from "@remix-run/react";
+import axios from "axios";
+import { FormEvent, useCallback, useState } from "react";
 
-export default function Login() {
-  const [apiKey, setApiKey] = useState<string | undefined>();
+const containerCss = css`
+  display: grid;
+  justify-content: center;
+  margin-top: 10vh;
+`;
 
-  useEffect(() => {
-    const storedApiKey = window.localStorage.getItem("apiKey");
-    if (storedApiKey) {
-      setApiKey(storedApiKey);
-    }
-  }, [setApiKey]);
+const formCss = css`
+  width: 250px;
 
-  const onFormSubmit = useCallback(() => {
-    if (apiKey) {
-      window.localStorage.setItem("apiKey", apiKey);
-    }
-  }, [apiKey]);
+  * {
+    display: block;
+  }
+
+  input {
+    border: 1px solid #d0d0d0;
+  }
+
+  > * {
+    margin-bottom: 14px;
+  }
+`;
+
+const fieldLabelCss = css`
+  margin-bottom: 2px;
+`;
+
+const submitCss = css`
+  background-color: #eaeaea;
+  padding: 4px 8px;
+`;
+
+export default function Login(): React.ReactElement {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+  const [error, setError] = useState<string | undefined>();
+
+  const onFormSubmit = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+
+      try {
+        const res = await axios.post("http://localhost:8000/auth/login/", {
+          username,
+          password,
+        });
+
+        if (typeof res.data?.token === "string") {
+          window.localStorage.setItem("token", res.data.token);
+          navigate("/");
+        } else {
+          setError("Something went wrong, try again");
+          setPassword("");
+        }
+      } catch {
+        setError("Something went wrong, try again");
+        setPassword("");
+      }
+    },
+    [username, password, navigate]
+  );
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <h2>Login</h2>
-      <label htmlFor="apiKey">API Key</label>
-      <input
-        type="text"
-        name="apiKey"
-        value={apiKey}
-        onChange={(e) => setApiKey(e.target.value)}
-      ></input>
-      <input type="submit" name="submit"></input>
-    </form>
+    <div css={containerCss}>
+      <form css={formCss} onSubmit={onFormSubmit}>
+        <h2>Login</h2>
+        <div>
+          <label css={fieldLabelCss} htmlFor="username">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e): void => setUsername(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label css={fieldLabelCss} htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e): void => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <input
+          css={submitCss}
+          type="submit"
+          name="submit"
+          value="Login"
+        ></input>
+        {error ? <span>{error}</span> : <></>}
+      </form>
+    </div>
   );
 }
