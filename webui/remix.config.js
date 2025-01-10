10fb868283d03d85de99ca9bae3fc2e9d8cbf2cf
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-10 19:20:37 +1100

    cleanup

--- webui/remix.config.js
+++ webui/remix.config.js
@@ -5,23 +5,15 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: 'cjs',
+  serverModuleFormat: "cjs",
   serverDependenciesToBundle: [
     /^@apollo\/client(\/.*)?$/,
     /^ol(\/.*)?/,
-    "ts-invariant", 
+    "ts-invariant",
     "zen-observable-ts",
     "@wry/equality",
     "@wry/trie",
     "optimism",
     "@wry/context",
   ],
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
-    v2_meta: true,
-    v2_headers: true,
-    v2_routeConvention: true
-  },
 };
