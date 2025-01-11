6ed8ffa3e8f5a22bee1c208e4e0022aba4711f11
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-11 13:20:34 +1100

    nicer favicon

--- webui/app/root.tsx
+++ webui/app/root.tsx
@@ -20,7 +20,7 @@ export const meta = (): MetaDescriptor[] => [
     charset: "utf-8",
   },
   {
-    title: "Howitt",
+    title: "Howitt Plains",
   },
   {
     name: "viewport",
@@ -150,5 +150,19 @@ export const links: LinksFunction = () => {
       href: "https://fonts.googleapis.com/css2?family=Hanken+Grotesk&display=swap",
     },
     { rel: "stylesheet", href: stylesheet },
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
   ];
 };
