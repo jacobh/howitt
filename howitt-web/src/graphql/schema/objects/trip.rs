8c202e2366a77563bb47b3f510bd875f1ba0c931
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-26 12:51:11 +1100

    expose further fields on a trip

--- howitt-web/src/graphql/schema/objects/trip.rs
+++ howitt-web/src/graphql/schema/objects/trip.rs
@@ -4,6 +4,8 @@ use howitt::models::{ride::RideFilter, trip::TripId};
 use crate::graphql::context::SchemaData;
 use crate::graphql::schema::{ride::Ride, ModelId};
 
+use super::user::UserProfile;
+
 pub struct Trip(pub howitt::models::trip::Trip);
 
 #[Object]
@@ -14,6 +16,29 @@ impl Trip {
 
     async fn name(&self) -> &str {
         &self.0.name
+    }
+
+    async fn year(&self) -> i32 {
+        self.0.year
+    }
+
+    async fn slug(&self) -> &str {
+        &self.0.slug
+    }
+
+    async fn description(&self) -> Option<&str> {
+        self.0.description.as_deref()
+    }
+
+    async fn user<'ctx>(&self, ctx: &Context<'ctx>) -> Result<UserProfile, async_graphql::Error> {
+        let SchemaData { user_loader, .. } = ctx.data()?;
+
+        let user = user_loader
+            .load_one(self.0.user_id)
+            .await?
+            .ok_or(anyhow::anyhow!("User not found"))?;
+
+        Ok(UserProfile(user))
     }
 
     async fn rides<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<Ride>, async_graphql::Error> {
