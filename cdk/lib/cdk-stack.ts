57705ceea36c3306ddb61ddc9da52bd67c214fdc
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-01-26 21:40:51 +1100

    remove unused vpc

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -70,10 +70,6 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = Vpc.fromLookup(this, "Vpc", {
-      isDefault: true,
-    });
-
     // ---
     // Dynamo
     // ---
