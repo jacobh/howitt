2f4caf9520bbf51d36e79ed8512c17c4eb9b10c9
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-02-01 23:41:03 +1100

    configure some lifecycle rules

--- cdk/lib/howitt-media-stack.ts
+++ cdk/lib/howitt-media-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Bucket, StorageClass } from "aws-cdk-lib/aws-s3";
 
 export class MediaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +19,31 @@ export class MediaStack extends cdk.Stack {
 
     const backupsBucket = new Bucket(this, "howitt-backups", {
       bucketName: "howitt-backups",
+      lifecycleRules: [
+        {
+          id: "PostgreSQL Daily Backups",
+          enabled: true,
+          prefix: "postgresql/daily/",
+          expiration: cdk.Duration.days(14),
+        },
+        {
+          id: "PostgreSQL Weekly Backups",
+          enabled: true,
+          prefix: "postgresql/weekly/",
+          expiration: cdk.Duration.days(45),
+        },
+        {
+          id: "PostgreSQL Monthly Backups",
+          enabled: true,
+          prefix: "postgresql/monthly/",
+          transitions: [
+            {
+              storageClass: StorageClass.GLACIER,
+              transitionAfter: cdk.Duration.days(1),
+            },
+          ],
+        },
+      ],
     });
   }
 }
