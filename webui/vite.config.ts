b89eb7a3a21286883b0c69c5d08ff9d0335c3334
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-02-02 20:28:55 +1100

    do some vite upgrading

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,15 +1,31 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-installGlobals();
+// declare module "@remix-run/node" {
+//   // or cloudflare, deno, etc.
+//   interface Future {
+//     v3_singleFetch: true;
+//   }
+// }
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        // v3_singleFetch: true,
+        // v3_routeConfig: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       lodash: "lodash-es",
