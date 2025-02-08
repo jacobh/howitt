3ebb1da1b11d307a1dcbdd609180509222f085b5
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-02-08 23:35:35 +1100

    fix eslint

--- webui/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "no-useless-constructor": "off",
-  },
-};
