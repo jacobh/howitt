99d6759cd55ba7a3c9c30a5a6aa7d6d1bdef5d8a
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-02-16 16:51:00 +1100

    process extension cli

--- src/bin/howitt-cli/src/commands/media.rs
+++ src/bin/howitt-cli/src/commands/media.rs
@@ -11,6 +11,10 @@ use crate::Context;
 #[derive(Subcommand)]
 pub enum MediaCommands {
     ProcessAll,
+    ProcessExtension {
+        #[clap(name = "extension")]
+        extension: String,
+    },
 }
 
 pub async fn handle(
@@ -34,6 +38,43 @@ pub async fn handle(
             }
 
             println!("Enqueued {} media items for processing", media.len());
+            Ok(())
+        }
+        MediaCommands::ProcessExtension { extension } => {
+            let media = media_repo.filter_models(MediaFilter::All).await?;
+            let extension = extension.to_lowercase();
+
+            println!("Scanning media items for .{} files...", extension);
+
+            let mut processed_count = 0;
+            for media_item in media.clone() {
+                // Extract the file extension from the path
+                if let Some(file_ext) = media_item
+                    .path
+                    .split('.')
+                    .last()
+                    .map(|ext| ext.to_lowercase())
+                {
+                    if file_ext == extension {
+                        job_storage
+                            .push(Job::from(MediaJob::Process(media_item.id)))
+                            .await?;
+
+                        println!(
+                            "Enqueued processing job for media {} ({})",
+                            media_item.id, media_item.path
+                        );
+                        processed_count += 1;
+                    }
+                }
+            }
+
+            println!(
+                "Found {} media items with .{} extension, enqueued {} for processing",
+                media.len(),
+                extension,
+                processed_count
+            );
             Ok(())
         }
     }
