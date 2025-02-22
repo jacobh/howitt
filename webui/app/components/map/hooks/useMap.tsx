4fa1315650ca844223c5c50cb5d74c97d2209034
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-02-22 15:43:30 +1100

    add logs + cleanup to useMap

--- webui/app/components/map/hooks/useMap.tsx
+++ webui/app/components/map/hooks/useMap.tsx
@@ -27,17 +27,26 @@ export function useMap({
   const [map, setMap] = useState<OlMap | undefined>(undefined);
 
   useEffect(() => {
+    console.log("[useMap] useMap useEffect triggered", {
+      existingMapInstance,
+      currentMap: map,
+      mapElementRef: mapElementRef.current,
+    });
+
     if (existingMapInstance) {
+      console.log("[useMap] Using existing map instance");
       setMap(existingMapInstance);
       existingMapInstance.setTarget(mapElementRef.current ?? undefined);
       return;
     }
 
     if (map) {
+      console.log("[useMap] Updating existing map target");
       map.setTarget(mapElementRef.current ?? undefined);
       return;
     }
 
+    console.log("[useMap] Creating new map instance");
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useGeographic();
 
@@ -57,12 +66,23 @@ export function useMap({
       interactions: [],
     });
 
+    console.log("[useMap] New map created", newMap);
     setMap(newMap);
     onNewMapInstance?.(newMap);
+
+    return () => {
+      console.log("[useMap] cleaning up", newMap);
+      newMap.setTarget(undefined);
+    };
   }, [existingMapInstance, mapElementRef, map, onNewMapInstance]);
 
   useEffect(() => {
     if (!map) return;
+
+    console.log("[useMap] Interaction effect triggered", {
+      interactive,
+      currentInteractions: map.getInteractions().getArray().length,
+    });
 
     // always reset the map to zero
     for (const interaction of map.getInteractions().getArray()) {
@@ -71,10 +91,16 @@ export function useMap({
 
     // then if interactive re-add controls
     if (interactive) {
+      console.log("[useMap] Adding default interactions");
       for (const interaction of defaultInteractions().getArray()) {
         map.addInteraction(interaction);
       }
     }
+
+    console.log(
+      "[useMap] Final interaction count:",
+      map.getInteractions().getArray().length,
+    );
   }, [map, interactive]);
 
   return { map };
