7de8325b16360dc036e3b6329e4a2d9d37c7debb
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-04-21 13:31:49 +1000

    add water beta table

--- src/lib/howitt-postgresql/migrations/V0025__osm_features.sql
+++ src/lib/howitt-postgresql/migrations/V0025__osm_features.sql
@@ -1,8 +1,16 @@
 CREATE TABLE osm_features (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     geometry GEOMETRY(GEOMETRY, 4326),
     name TEXT,
     attributes JSONB
 );
 
 CREATE INDEX osm_features_geom_idx ON osm_features USING GIST(geometry);
+
+CREATE TABLE water_beta (
+    id UUID PRIMARY KEY,
+    osm_feature_id BIGINT REFERENCES osm_features(id),
+    attributes JSONB
+);
+
+CREATE INDEX water_beta_osm_feature_id_idx ON water_beta(osm_feature_id);
