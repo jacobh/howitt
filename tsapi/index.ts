6291b1e1130460c533de6cd3da944397e059b6a2
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-04-21 14:14:46 +1000

    simple example query

--- tsapi/index.ts
+++ tsapi/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
+import { sql } from "bun";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+const exampleQuery = sql`
+    select count(*) from osm_features;
+`;
+
+app.get("/", async (c) => {
+  const res = await exampleQuery.execute();
+
+  return c.json(res);
+});
 
 export default {
   port: 3001,
